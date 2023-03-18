8a6f987257a63ee6983ee1e37cd86867e571f006
Eric Poroznik <ericporoznik@gmail.com>
2023-03-18 14:41:46 -0400

    added first command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -6,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 intents = discord.Intents.all()
 
-bot = discord.Client(
+bot = commands.Bot(
     command_prefix="$",
     help_command=None,
     intents=intents,
@@ -15,5 +16,9 @@ bot = discord.Client(
 @bot.event
 async def on_ready():
     print(f"Logged in!")
+
+@bot.command()
+async def test(ctx):
+    await ctx.reply("Test Complete.")
 
 bot.run(os.getenv("TOKEN"))
