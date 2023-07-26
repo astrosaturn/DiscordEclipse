35375ed5484091ae428fb4c9c58731816432d7e6
Saturn <ericporoznik@gmail.com>
2023-07-26 18:55:27 -0400

    Moved all DB logic to profilemanager

--- Cogs/levels.py
+++ Cogs/levels.py
@@ -1,23 +1,9 @@
 import discord
-import os
-import mariadb
 from discord.ext import commands
 import random 
 from random import *
 import math
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user=os.getenv("USER"),
-        password=os.getenv("PASSWORD"),
-        host=os.getenv("HOST"),
-        port=int(os.getenv("PORT")),
-        database=os.getenv("DATABASE")
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform in levels.py: {e}")
-
-cur = conn.cursor()
+from profilemanager import *
 
 
 class Levels(commands.Cog):
@@ -39,72 +25,41 @@ class Levels(commands.Cog):
         
         member_id = message.author.id
         
-        cur.execute(
-            "SELECT user_id FROM users WHERE user_id=?", (member_id,)            
-        )
-        member = cur.fetchone()
+        member = check_db_for_user(member_id)
 
         #If the user ID is not in the database, add it.
         if member == None:       
-            cur.execute("INSERT INTO users (user_id, level, current_xp) VALUES (?, 1, 100)", (member_id,))
+            initiate_user(member_id)
         
         else:
-            cur.execute(
-                "SELECT current_xp FROM users WHERE user_id=?", (member_id,)
-            )
-            member_xp = cur.fetchone()
-
-            cur.execute(
-                "SELECT level FROM users WHERE user_id=?", (member_id,)
-            )
-            member_level = cur.fetchone()
-
-            xp_amount = randint(3, 10)
-            xp_to_give = int(member_xp[0]) + xp_amount
-            cur.execute(
-                "UPDATE users SET current_xp = ? WHERE user_id=?", (xp_to_give, member_id,)
-            )
+            member_xp = get_user_xp(member_id)
+            member_level = get_user_level(member_id)
             
-            #God I fucking hate MySQL           
-            member_level = int(member_level[0])
+            
+            xp_amount = randint(3, 10)      # Pick a random value
+            xp_to_give = member_xp + xp_amount      # Add together
+            set_user_xp(xp_to_give, member_id)      # Give the user XP
 
             xp_to_level_up = math.floor(100*(1.10) ** member_level)
-            if int(member_xp[0]) >= xp_to_level_up:
-                member_new_level = member_level + 1
-                cur.execute(
-                    "UPDATE users SET level=? WHERE user_id=?", (member_new_level, member_id,)                   
-                )
-                cur.execute(
-                    "UPDATE users SET current_xp = 0 WHERE user_id=?", (member_id,)
-                )
+            if member_xp >= xp_to_level_up:
+                member_new_level = member_level + 1  
+                level_set(member_new_level, member_id)      # Level the user up
+                set_user_xp(0, member_id)       # Reset their XP back to 0
                 await message.channel.send(f"<@{message.author.id}>, you have leveled up to level {member_new_level}!")   
                 return            
-        conn.commit()
     
     @commands.command()
     async def xp(self, ctx):
         author_id = ctx.author.id
-        cur.execute(
-            "SELECT current_xp FROM users WHERE user_id=?", (author_id,)
-        )
-        author_xp = cur.fetchone()
-        author_xp = int(author_xp[0])
+        author_xp = get_user_xp(author_id)
         await ctx.reply(f"You have {author_xp} XP!")
 
     @commands.command()
     async def level(self, ctx):
         author_id = ctx.author.id
-        cur.execute(
-            "SELECT level FROM users WHERE user_id=?", (author_id,)
-        )
-        author_level = cur.fetchone()
-        author_level = int(author_level[0])
+        author_level = get_user_level(author_id)
         xp_to_level_up = math.floor(100*(1.30) ** author_level)
-        cur.execute(
-            "SELECT current_xp FROM users WHERE user_id=?", (author_id,)    
-        )
-        author_xp = cur.fetchone()
-        author_xp = int(author_xp[0])
+        author_xp = get_user_xp(author_id)
 
         total_xp_to_levelup = xp_to_level_up - author_xp
         if total_xp_to_levelup < 0:
