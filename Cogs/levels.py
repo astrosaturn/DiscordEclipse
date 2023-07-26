f03b0ad005282b1d9d76d540e6970093f06668a0
Eric Poroznik <ericporoznik@gmail.com>
2023-07-26 00:15:39 -0400

    OH MY FUCKING GOD LEVELS WORK OH MY GOODDDDDD

--- Cogs/levels.py
+++ Cogs/levels.py
@@ -5,6 +5,8 @@ import sys
 from discord.ext import commands
 import random 
 from random import *
+import math
+import time
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
@@ -34,27 +36,84 @@ class Levels(commands.Cog):
     #Get XP per message sent.
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.author.id == 485057961358524427:
+            return
+        
         member_id = message.author.id
         
         cur.execute(
-            "SELECT user_id FROM users WHERE user_id=?",(member_id,)
+            "SELECT user_id FROM users WHERE user_id=?", (member_id,)            
         )
         member = cur.fetchone()
 
-        if member == None:
-            cur.execute(
-                "INSERT INTO users (user_id, level, current_xp) VALUES (?, 0, 0)", (member_id,)
-            )
+        #If the user ID is not in the database, add it.
+        if member == None:       
+            cur.execute("INSERT INTO users (user_id, level, current_xp) VALUES (?, 1, 100)", (member_id,))
+        
         else:
             cur.execute(
                 "SELECT current_xp FROM users WHERE user_id=?", (member_id,)
             )
             member_xp = cur.fetchone()
-            xp_amount = randint(10, 35)
-            xp_to_give = member_xp + xp_amount
-            cur.exectue(
-                "UPDATE users SET current_xp = ?", (xp_to_give,)
+
+            cur.execute(
+                "SELECT level FROM users WHERE user_id=?", (member_id,)
             )
+            member_level = cur.fetchone()
+
+            xp_amount = randint(3, 10)
+            xp_to_give = int(member_xp[0]) + xp_amount
+            cur.execute(
+                "UPDATE users SET current_xp = ? WHERE user_id=?", (xp_to_give, member_id,)
+            )
+            
+            #God I fucking hate MySQL           
+            member_level = int(member_level[0])
+
+            xp_to_level_up = math.floor(100*(1.10) ** member_level)
+            if int(member_xp[0]) >= xp_to_level_up:
+                member_new_level = member_level + 1
+                cur.execute(
+                    "UPDATE users SET level=? WHERE user_id=?", (member_new_level, member_id,)                   
+                )
+                cur.execute(
+                    "UPDATE users SET current_xp = 0 WHERE user_id=?", (member_id,)
+                )
+                await message.channel.send(f"<@{message.author.id}>, you have leveled up to level {member_new_level}!")   
+                return            
+        conn.commit()
+    
+    @commands.command()
+    async def xp(self, ctx):
+        author_id = ctx.author.id
+        cur.execute(
+            "SELECT current_xp FROM users WHERE user_id=?", (author_id,)
+        )
+        author_xp = cur.fetchone()
+        author_xp = int(author_xp[0])
+        await ctx.reply(f"You have {author_xp} XP!")
+
+    @commands.command()
+    async def level(self, ctx):
+        author_id = ctx.author.id
+        cur.execute(
+            "SELECT level FROM users WHERE user_id=?", (author_id,)
+        )
+        author_level = cur.fetchone()
+        author_level = int(author_level[0])
+        xp_to_level_up = math.floor(100*(1.10) ** author_level)
+        cur.execute(
+            "SELECT current_xp FROM users WHERE user_id=?", (author_id,)    
+        )
+        author_xp = cur.fetchone()
+        author_xp = int(author_xp[0])
+
+        total_xp_to_levelup = xp_to_level_up - author_xp
+        if total_xp_to_levelup < 0:
+            total_xp_to_levelup = 0
+        await ctx.reply(f"You are level `{author_level}`. You have `{xp_to_level_up - author_xp}` XP left until `{author_level + 1}`.")
+
+
 
 async def setup(bot):
     await bot.add_cog(Levels(bot))
